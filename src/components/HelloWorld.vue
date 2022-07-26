5dfd92de9499c9569fa3a52dfe759a0129205bce
Deepika1261 <57223834+Deepika1261@users.noreply.github.com>
2022-07-26 16:23:06 +0530

    notes app with vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <v-toolbar flat app>
+    <v-toolbar-title class="display-2 grey--text">
+      <span class="font-weight-light">Add</span>
+      <span>Notes</span>
+    </v-toolbar-title>
+  </v-toolbar>
+    <v-container>
+      <v-row class="mb-6">
+        <v-col cols="12">
+          <v-card class="pa-4" outlined tile color="indigo lighten-4" :elevation=6>
+            <form>
+              <v-text-field v-model="$store.state.note_heading" label="HEADING" required></v-text-field>
+              <v-text-field v-model="$store.state.note" label="BODY" required></v-text-field>
+              <v-btn depressed color="primary" class="mr-5" @click="$store.commit('addNote')">Add</v-btn>
+              <v-btn depressed color="error">Clear</v-btn>
+            </form>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-flex d-flex>
+        <v-layout wrap>
+            <v-flex md4 v-for="note in $store.state.notes" :key="note.id">
+              <v-hover class="mb-4">
+                <template v-slot:default="{ hover }">
+                <v-card color="indigo lighten-4" max-width="350" height="300" :elevation="hover ? 24 : 6" class="mx-auto">
+                <v-card-title class="pa-1 ma-2">
+                <v-row justify="space-between" class="pa-3">
+                  <v-btn text icon>
+                    note
+                  </v-btn>
+                  <v-btn text icon @click="$store.commit('deleteNote',{the_note:note})">
+                    <v-icon dark>
+                      mdi-delete
+                    </v-icon>
+                  </v-btn>
+                </v-row>
+                <input v-if="$store.state.selected_note!=null && $store.state.selected_note.id==note.id" v-model="$store.state.selected_note.note_heading" class="body-plain" style="display:block;">
+                <v-card-text v-else class="pa-0 ma-0">
+                <div class=" title text-truncate">{{ note.note_heading }}</div>
+                </v-card-text>
+              </v-card-title>
+              <v-textarea
+                v-if="$store.state.selected_note!=null && $store.state.selected_note.id==note.id"
+                solo
+                name="input-7-4"
+                label="Solo textarea"
+                v-model="$store.state.selected_note.note"
+                style="height=160px;">
+              </v-textarea>
+              <v-card-text v-else class="body-1 font-weight-medium" style=" height:160px; overflow-y: scroll">
+                {{ note.note }}
+              </v-card-text>
+              <v-card-actions>
+                <v-list-item class="grow">
+                  <v-row justify="center">
+                  <v-btn v-if="$store.state.selected_note==null" class="ma-2" outlined @click="$store.commit('editNote',{the_note:note})">Edit Note</v-btn>
+                  <v-btn v-if="$store.state.selected_note!=null && $store.state.selected_note.id==note.id" class="ma-2 mr-5" outlined @click="$store.commit('submitNote',{note:note})">Update</v-btn>
+                  <v-btn v-if="$store.state.selected_note!=null && $store.state.selected_note.id==note.id" class="ma-2 error" outlined @click="$store.commit('discardNote')">Discard</v-btn>
+                  </v-row>
+                </v-list-item>
+            </v-card-actions>
+            </v-card>
+            </template>
+            </v-hover>
+            </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-container>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,45 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    note:"",
+    note_heading:"",
+    id:0,
+    notes:[],
+    selected_note:null
   },
   getters: {
   },
   mutations: {
+    reset(state){
+      state.note = '',
+      state.note_heading = ''
+      state.id=state.id+1
+    },
+    addNote(state){
+      let my_note={
+          note_heading:state.note_heading,
+          note:state.note,
+          id: state.id,
+      }
+      state.notes.push(my_note)
+      console.log(state.notes[0])
+      this.commit('reset')
+    },
+    deleteNote(state,payload){
+      const idx = state.notes.indexOf(payload.the_note)
+      state.notes.splice(idx,1)
+    },
+    editNote(state,payload){
+      state.selected_note = JSON.parse(JSON.stringify(payload.the_note));
+    },
+    submitNote(state,payload){
+      payload.note.note_heading=state.selected_note.note_heading;
+      payload.note.note=state.selected_note.note;
+      state.selected_note=null;
+    },
+    discardNote(state){
+      state.selected_note=null;
+    }
   },
   actions: {
   },
